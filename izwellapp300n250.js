4ec7bf7b079e65cc43aac650587de99908a82d33
randomap1 <118092966+randomap1@users.noreply.github.com>
2022-12-01 17:02:25 +0700

    Create izwellapp300n250.js

---
+++ izwellapp300n250.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '8dc1f8a79bc63e193f24c5509d315fe5',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/8dc1f8a79bc63e193f24c5509d315fe5/invoke.js"></scr' + 'ipt>');
