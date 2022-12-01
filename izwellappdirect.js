20170699c5bfa9fd8565ae5af00fc7d3cbcbb4ad
randomap1 <118092966+randomap1@users.noreply.github.com>
2022-12-01 18:27:31 +0700

    Create izwellappdirect.js

---
+++ izwellappdirect.js
@@ -0,0 +1,12 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://capricedes.com/mhzzq54w0b?key=08bbca04c13b15a6acac359750efe899', '_blank');
+	});
