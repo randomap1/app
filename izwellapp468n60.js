88b42d017049f236789932dc8a17390bcf4c4f1d
randomap1 <118092966+randomap1@users.noreply.github.com>
2022-12-01 18:23:18 +0700

    Create izwellapp468n60.js

---
+++ izwellapp468n60.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '6e30f4f7c3d520471d5b9770c4ef9d0e',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/6e30f4f7c3d520471d5b9770c4ef9d0e/invoke.js"></scr' + 'ipt>');
