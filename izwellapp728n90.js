8459e0dc709d34bbb66ed59fa208a681f04a24ca
randomap1 <118092966+randomap1@users.noreply.github.com>
2022-12-01 17:07:15 +0700

    Create izwellapp728n90.js

---
+++ izwellapp728n90.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '0ddfbb0b4673146e63bc5c5b1d6bb92b',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/0ddfbb0b4673146e63bc5c5b1d6bb92b/invoke.js"></scr' + 'ipt>');
